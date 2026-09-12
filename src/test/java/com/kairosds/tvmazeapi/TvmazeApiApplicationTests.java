9b9bf7c08936ea539d62a232696d34c207a9e625
Alonso Uribe <alonsouribe09@gmail.com>
2026-09-12 00:11:39 -0700

    proyecto inicial

---
+++ src/test/java/com/kairosds/tvmazeapi/TvmazeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairosds.tvmazeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvmazeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
